51f7e4820804c68e64d6b510395977584b8e5970
Jimbow <adeathjohn@gmail.com>
2014-06-02 21:16:12 +0800

    sdfas

--- TrnthMotion.cs
+++ TrnthMotion.cs
@@ -2,12 +2,31 @@
 using System.Collections;
 
 public class TrnthMotion : MonoBehaviour {
-	public Animator animator;
-	public TrnthCharacterControllerCreature ccc;
+	public TrnthMotionExecuter executor;
+	public int priority;
 	public float delayAnimator;
 	public float delayForce;
 	public float cooldown;
 	public string animatorParameter;
 	public Vector3 forceWorld;
 	public Vector3 forceLocal;
+	public TrnthAntenna[] antennasNeeded;
+	public TrnthAntenna[] antennasFree;
+	bool isOn(){
+		foreach(TrnthAntenna e in antennasNeeded){
+			if(!e.isTriggerStay)return false;
+		}
+		return true;
+	}
+	bool isOff(){
+		foreach(TrnthAntenna e in antennasFree){
+			if(e.isTriggerStay)return false;
+		}
+		return true;
+	}
+	void OnTriggerStay(){
+		if(!isOff())return;
+		if(!isOn())return;
+		executor.add(this);
+	}
 }
--- TrnthMotionExecuter.cs
+++ TrnthMotionExecuter.cs
@@ -1,9 +1,45 @@
 ﻿using UnityEngine;
-using System.Collections;
-
-public class TrnthMotionExecuter : MonoBehaviour {
+using System.Collections.Generic;
+using TRNTH;
+public class TrnthMotionExecuter : TRNTH.MonoBehaviour {
 	public Animator animator;
 	public TrnthCharacterControllerCreature ccc;
-	public float delay
-	public float cooldown;
+	public void add(TrnthMotion motion){
+		if(!a.a)return;
+		enabled=true;
+		list.Add(motion);
+	}
+	public void execute(TrnthMotion motion){
+		if(!motion)return;
+		if(!a.a)return;
+		Invoke("_force",motion.delayForce);
+		Invoke("_animator",motion.delayAnimator);
+		a.s=motion.cooldown;		
+	}
+	public TrnthMotion chooseMotion(){
+		if(list.Count<1)return null;
+		motion=list[0];
+		foreach(TrnthMotion e in list.ToArray()){
+			if(e.priority>motion.priority)motion=e;
+		}
+		return motion;
+	} 
+	public void clear(){
+		list.Clear();
+		enabled=false;
+	}
+	TrnthMotion motion;
+	List<TrnthMotion> list=new List<TrnthMotion>();
+	Alarm a=new Alarm();
+	void _force(){
+		ccc.vecForce=motion.forceWorld+ccc.transform.TransformDirection(motion.forceLocal);
+	}
+	void _animator(){
+		animator.SetTrigger(motion.animatorParameter);
+	}	
+	void Update(){
+		motion=chooseMotion();
+		execute(motion);
+		clear();
+	}
 }
