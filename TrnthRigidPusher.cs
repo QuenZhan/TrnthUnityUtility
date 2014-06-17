85eda98405f32394af4054f84c5936cd56014eec
Jim Mac mini <jimzheng@voo.com.tw>
2014-06-17 17:56:22 +0800

    sdfaf

---
+++ TrnthRigidPusher.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrnthRigidPusher : TrnthMonoBehaviour {
+	public string collideTag;
+	public float force=10;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnControllerColliderHit(ControllerColliderHit hit){
+		return;
+		var rigid=hit.rigidbody;
+		if(rigid){
+			var vec=rigid.transform.position-pos;
+			rigid.AddForce(vec.normalized*force);
+
+		}
+
+	}
+	void OnTriggerStay(Collider col){
+		if(col.gameObject.tag==collideTag){
+			var rigid=col.transform.parent.rigidbody;
+			var vec=rigid.transform.position-pos;
+			rigid.AddForce(vec.normalized*force);
+
+		}
+	}
+}
