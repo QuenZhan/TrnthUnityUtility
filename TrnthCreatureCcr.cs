a6dd0192aba9861b999f78c2342ebb6bf9bc6917
Jimbow <adeathjohn@gmail.com>
2014-07-07 23:19:01 +0800

    sdfasf

--- TrnthCreatureCcr.cs
+++ TrnthCreatureCcr.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using TRNTH;
 public class TrnthCreatureCcr:TrnthCreature{
 	public CharacterController ccr;
+	public float fSpacer;
 	public bool walkInTheAir;
 	void FixedUpdate(){
 		float dt=Time.deltaTime;
@@ -22,5 +23,10 @@ public class TrnthCreatureCcr:TrnthCreature{
 		if ((flag & CollisionFlags.Above) != 0
 			&&vecForce.y>0)vecForce.y=0;
 
+	}
+	void OnControllerColliderHit(ControllerColliderHit hit){
+		var vec=-hit.moveDirection;
+		vec.y=0;
+		vecForce+=vec*fSpacer;
 	}
 }
