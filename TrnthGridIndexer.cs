dce849f1c3af8e554493ac29be15ea379182800b
Jim Mac mini <jimzheng@voo.com.tw>
2014-07-25 18:09:12 +0800

    蛤？

--- TrnthGridIndexer.cs
+++ TrnthGridIndexer.cs
@@ -2,8 +2,10 @@
 using System.Collections;
 [ExecuteInEditMode]
 public class TrnthGridIndexer : TrnthMonoBehaviour {
+	public enum Directioin{ down,left,right,up}
 	public int index;
 	public int margin=600;
+	public Directioin directioin;
 	public float orgin=300;
 	public float rate=0.2f;
 	[ContextMenu("execute")]
@@ -13,7 +15,18 @@ public class TrnthGridIndexer : TrnthMonoBehaviour {
 	public void update(float rate){
 		var yy=orgin+index*margin;
 		var vec=tra.localPosition;
-		vec.y+=(yy-vec.y)*rate;
+		switch(directioin){
+		case Directioin.down:
+			vec.y+=(yy-vec.y)*rate;	
+			break;
+		case Directioin.left:
+			vec.x+=(yy-vec.x)*rate;
+			break;
+		case Directioin.right:
+			yy=orgin-index*margin;
+			vec.x+=(yy-vec.x)*rate;
+			break;
+		}
 		tra.localPosition=vec;
 	}
 	void Update(){
