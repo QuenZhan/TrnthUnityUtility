bec943e3be8067148996bfdc0625ab9ae7350a71
Jim Mac mini <jimzheng@voo.com.tw>
2014-07-25 15:46:21 +0800

    sdf

---
+++ TrnthGridIndexer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+[ExecuteInEditMode]
+public class TrnthGridIndexer : TrnthMonoBehaviour {
+	public int index;
+	public int margin=600;
+	public float orgin=300;
+	public float rate=0.2f;
+	[ContextMenu("execute")]
+	public void execute(){
+		update(1);
+	}
+	public void update(float rate){
+		var yy=orgin+index*margin;
+		var vec=tra.localPosition;
+		vec.y+=(yy-vec.y)*rate;
+		tra.localPosition=vec;
+	}
+	void Update(){
+		update(rate);
+	}
+}
