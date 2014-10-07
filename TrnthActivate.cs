0a9e0161d047790fdaadd0a9ee8ddfd2a0066039
Jimbow <adeathjohn@gmail.com>
2014-10-07 08:59:28 +0800

    Activator GridIndexer ScrollPager

---
+++ TrnthActivate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrnthActivate : MonoBehaviour {
+	public GameObject target;
+	public float delay=1;
+	public bool toggle;
+	public void execute(){
+		target.SetActive(toggle);
+	}
+	void OnEnable () {
+		Invoke("execute",delay);
+	}
+	void Awake(){
+		if(!target)target=gameObject;
+	}
+}
--- TrnthGridIndexer.cs
+++ TrnthGridIndexer.cs
@@ -1,10 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
-[ExecuteInEditMode]
 public class TrnthGridIndexer : TrnthMonoBehaviour {
 	public enum Directioin{ down,left,right,up}
 	public int index;
 	public int margin=600;
+	public int length=0;
 	public Directioin directioin;
 	public float orgin=300;
 	public float rate=0.2f;
@@ -13,6 +13,7 @@ public class TrnthGridIndexer : TrnthMonoBehaviour {
 		update(1);
 	}
 	public void update(float rate){
+		clamp();
 		var yy=orgin+index*margin;
 		var vec=tra.localPosition;
 		switch(directioin){
@@ -28,6 +29,24 @@ public class TrnthGridIndexer : TrnthMonoBehaviour {
 			break;
 		}
 		tra.localPosition=vec;
+	}
+	public void setRounded(){
+		var delta=0f;
+		switch(directioin){
+		case Directioin.down	:delta=-tra.localPosition.y-orgin;break;
+		case Directioin.up		:delta=tra.localPosition.y-orgin;break;
+		case Directioin.right	:delta=-tra.localPosition.x-orgin;break;
+		case Directioin.left	:delta=tra.localPosition.x-orgin;break;
+		}
+		// Debug.Log(delta);
+		index=(int)Mathf.Round(delta/margin);
+		clamp();
+	}
+	public void clamp(){
+		if(length!=0){
+			if(index<0)index=0;
+			if(index>=length)index=length-1;
+		}
 	}
 	void Update(){
 		update(rate);
