220207edb5ae7208bc792a94df76f93f11121019
JimbowMBPR <adeathjohn@gmail.com>
2014-12-09 15:45:58 +0800

    sdfs

--- TrnthInputDragRotate.cs
+++ TrnthInputDragRotate.cs
@@ -4,12 +4,13 @@ using System.Collections;
 public class TrnthInputDragRotate : MonoBehaviour {
 	public int mouseButton=1;
 	public float clickThreshold=0.2f;
+	public float rate=1;
 	public string state;
 	void Update(){
 		switch(state){
 		case"draging":
 			var delta=Input.mousePosition-_coor;
-			transform.localEulerAngles=_angles+(new Vector3(-delta.y,delta.x,0));
+			transform.localEulerAngles=_angles+(new Vector3(-delta.y,delta.x,0))*rate;
 			if(Input.GetMouseButtonUp(mouseButton)){
 				if(!a.a){
 					transform.localEulerAngles=Vector3.zero;
--- TrnthLookAt.cs
+++ TrnthLookAt.cs
@@ -1,14 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
-[ExecuteInEditMode]
+// [ExecuteInEditMode]
 public class TrnthLookAt : MonoBehaviour {
 	public Transform self;
 	public Transform target;
+	public string findTarget;
 	public bool yOnly=false;
+	void find(){
+		var go=GameObject.Find(findTarget);
+		if(go)target=go.transform;
+	}
+	[ContextMenu("update")]
 	void Update () {
 		if(!target)return;
 		var vec=target.position;
 		if(yOnly)vec.y=self.position.y;
 		self.LookAt(vec);
+	}
+	void OnEnable(){
+		if(!target)find();
+		Update();
 	}
 }
