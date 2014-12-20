eaf9d07ceba569bd1740846da713f74610c19b76
JimbowMBPR <adeathjohn@gmail.com>
2014-12-20 08:13:26 +0800

    dSDF

---
+++ TrnthModelBase.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrnthModelBase : MonoBehaviour {
+	public bool applyOnAwake=true;
+	[ContextMenu("apply")]
+	public void applyContextMenu(){
+		apply();
+	}
+	public virtual void apply(){
+		
+	}
+	void Awake(){
+		if(applyOnAwake)apply();
+	}
+}
