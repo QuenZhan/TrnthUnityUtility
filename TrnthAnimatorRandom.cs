1c87cedb974d3bd45a36db2a29cc969b1c367533
JimbowMBPR <adeathjohn@gmail.com>
2015-01-03 18:35:49 +0800

    random float steps

--- TrnthAnimatorRandom.cs
+++ TrnthAnimatorRandom.cs
@@ -3,7 +3,13 @@ using System.Collections;
 
 public class TrnthAnimatorRandom : TrnthAnimator {
 	// public bool yes;
+	public int steps;
 	public override void execute(){
-		animator.SetFloat(parameterName,Random.value);
+		if(steps==0){
+			animator.SetFloat(parameterName,Random.value);
+		}else{
+			var value=Mathf.Floor(Random.value*steps)/steps;
+			animator.SetFloat(parameterName,value);
+		}
 	}
 }
