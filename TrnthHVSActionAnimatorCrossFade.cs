70c4633cfeeb5738ffe9d6fa4419a24dead36cd8
JimbowMBPR <adeathjohn@gmail.com>
2015-01-07 01:15:41 +0800

    sf

---
+++ TrnthHVSActionAnimatorCrossFade.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrnthHVSActionAnimatorCrossFade : TrnthHVSActionAnimator {
+	public string stateName;
+	public float transitionDuration=0.7f;
+	protected override void _execute(){
+		base._execute();
+		if(variable)animator=variable.read<Animator>();
+		animator.CrossFade(stateName,transitionDuration);
+	}
+}
