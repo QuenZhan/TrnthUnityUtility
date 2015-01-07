7a7291b15cc37446f58ca0d22d614f09aec81caf
JimbowMBPR <adeathjohn@gmail.com>
2015-01-08 03:51:50 +0800

    sd

--- TrnthHVSActionPositionSet.cs
+++ TrnthHVSActionPositionSet.cs
@@ -3,18 +3,20 @@ using System.Collections;
 
 public class TrnthHVSActionPositionSet : TrnthHVSAction {
 	public Transform target;
-	public UnityEngine.Space space=Space.Self;
+	// public UnityEngine.Space space=Space.Self;
 	public Vector3 pos;
 	public Transform posWorld;
+	public Vector3 noise;
 	protected override void _execute(){
-		switch(space){
-		case Space.Self:
-			target.localPosition=pos;
-			break;
-		case Space.World:
-			target.position=pos;
-			break;
-		}
-		if(posWorld)target.position=posWorld.position;
+		var pos=this.pos+noise*Random.value;
+		target.localPosition=pos;
+		// switch(space){
+		// case Space.Self:
+		// 	break;
+		// case Space.World:
+		// 	target.position=pos;
+		// 	break;
+		// }
+		if(posWorld)target.position=posWorld.position+noise*Random.value;
 	}
 }
