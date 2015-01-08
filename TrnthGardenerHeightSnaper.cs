c18951f7ac5b87d3e54fdbfa2eadaf3ecd5861ed
JimbowMBPR <adeathjohn@gmail.com>
2015-01-09 03:40:36 +0800

    Gardener Height

---
+++ TrnthGardenerHeightSnaper.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrnthGardenerHeightSnaper : MonoBehaviour {
+	public Transform group;
+	public Vector3 offset;
+	// public LayerMask layerMask ;
+	[ContextMenu("execute")]
+	public void execute(){
+		foreach(Transform e in group){
+			var pos=e.position;
+			pos.y=300;
+			RaycastHit hitInfo;
+			if(Physics.Raycast(pos,Vector3.up*-1,out hitInfo)){
+				e.position=hitInfo.point+offset;
+			}
+			// e.positioin
+		}
+	}
+}
