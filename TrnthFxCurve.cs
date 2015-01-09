d1c51a7a8fb7082c46522c27a105a88e30e4c127
JimbowMBPR <adeathjohn@gmail.com>
2015-01-10 05:01:16 +0800

    fx 系列

---
+++ TrnthFxCurve.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using TRNTH;
+public class TrnthFxCurve:MonoBehaviour{
+	public string findIt;
+	public AnimationCurve curve;
+	public float noise;
+	public virtual void start(){
+		enabled=true;		
+		_timeStart=Time.time;
+		var keys=curve.keys;
+		var lastkey=keys[keys.Length-1];
+		Invoke("end",lastkey.time);
+	}
+	protected virtual void update(){
+	}
+	protected virtual float curveValue{get{return curve.Evaluate(Time.time-_timeStart)+Random.value*noise;}}
+	float _timeStart;
+	void end(){
+		enabled=false;
+	}
+	void OnEnable(){
+		start();
+	}
+	void OnDisable(){
+		CancelInvoke();
+	}
+	void Update(){
+		update();
+	}
+}
---
+++ TrnthFxCurveAudio.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrnthFxCurveAudio : TrnthFxCurve {
+	public AudioSource audioSource;
+	protected override void update(){
+		audioSource.volume=curveValue;
+	}
+}
