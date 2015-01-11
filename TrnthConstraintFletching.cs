9ca8c4b99ba9a2e418256b46bf92c645de2f5ea2
JimbowMBPR <adeathjohn@gmail.com>
2015-01-12 07:08:13 +0800

    fletching emit burst

--- TrnthConstraintFletching.cs
+++ TrnthConstraintFletching.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class TrnthConstraintFletching : MonoBehaviour {
 	public Rigidbody rig;
 	public bool is2d;
+	public bool angleClamp=true;
 	void Update(){
 		// var vecUp=Vector3.up;
 		if(is2d){
@@ -13,6 +14,12 @@ public class TrnthConstraintFletching : MonoBehaviour {
 			// var value=vec.x!=0?(vec.y/vec.x):(Mathf.Infinity);
 			// if(vec.x==0)
 			var angle=Vector3.Angle(Vector3.up,vec);
+			if(angleClamp){
+				angle/=3f;
+				angle+=60;
+				// if(angle<60)angle=60;
+				// if(angle>120)angle=120;
+			}			
 			if(vec.x>0)angle*=-1;
 			transform.eulerAngles=Vector3.forward*angle;
 			// vecUp=Vector3.right;
---
+++ TrnthHVSActionParticleEmitBurst.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrnthHVSActionParticleEmitBurst : TrnthHVSAction {
+	public ParticleSystem particle;
+	public int number;
+	public int noise;
+	protected override void _execute(){
+		base._execute();
+		particle.Emit((int)(number+Random.value*noise));
+	}
+}
