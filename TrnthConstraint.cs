2a634ac11fa67f1111431dcbf72e9642e23ec979
JimbowMBPR <adeathjohn@gmail.com>
2015-02-04 01:57:15 +0800

    sf

---
+++ TrnthConstraint.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrnthConstraint : MonoBehaviour {
+	public Transform target;
+	public virtual void setup(){
+		if(!target)target=transform;
+	}
+	public virtual void update(){
+
+	}
+	[ContextMenu("update")]
+	public void contextMenuUpdate(){
+		update();
+	}
+	void Awake(){
+		setup();
+	}
+	void Update(){
+		update();
+	}
+}
