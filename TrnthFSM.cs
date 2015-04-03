72abac531fe81fc74407c69779f68732964bcfe5
JImbowMBAir <adeathjohn@gmail.com>
2015-04-04 00:22:48 +0800

    fix

--- TrnthFSM.cs
+++ TrnthFSM.cs
@@ -3,6 +3,11 @@ using System.Collections;
 using System.Linq;
 
 public class TrnthFSM_ : MonoBehaviour {
+	// static public void transit(GameObject gameObject){
+	// 	var fsmManager=state.transform.parent.GetComponent<TrnthFSMManager>();
+	// 	fsmManager.stateNow=state;
+	// 	fsmManager.update();
+	// }
 	public GameObject stateNow;
 	[ContextMenu("update")]
 	public virtual void update(){
@@ -16,7 +21,8 @@ public class TrnthFSM_ : MonoBehaviour {
 	}
 	public virtual void transit(string name){
 		foreach(Transform e in transform.Cast<Transform>().ToArray()){
-			e.gameObject.SetActive(e.name==name);
+			if(e.name==name)stateNow=e.gameObject;
+			e.gameObject.SetActive(e.name==name);			
 		}
 	}
 	void Start(){
