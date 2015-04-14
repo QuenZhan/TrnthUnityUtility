443e58543fb8c51c85d158c2b95ef73321069214
JimbowMBPR <adeathjohn@icloud.com>
2015-04-15 02:16:15 +0800

    sliding generator

--- TrnthFxSlidingNumberGenerator.cs
+++ TrnthFxSlidingNumberGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 [RequireComponent(typeof(TrnthFxSlidingNumber))]
 public class TrnthFxSlidingNumberGenerator:MonoBehaviour{
@@ -8,20 +9,29 @@ public class TrnthFxSlidingNumberGenerator:MonoBehaviour{
 	public float marginDigit=20;
 	public float marginNumber=40;
 	public int digits;
-	public UI.Text prefab;
+	public UnityEngine.UI.Text prefab;
 	[ContextMenu("generate")]
-	public void generate{
+	public void generate(){
 		TRNTH.U.cleanChildren(transform);
-		var slidingNumber=GetComponent<TrnthFxSlidingNumber>();
+		var list=new List<TrnthFxIndexer>();
 		for(int i=0;i<digits;i++){
 			var group = new GameObject("digitGroup");
 			group.transform.parent=this.transform;
-			group.transform.localPosition=new Vecto3(i*marginDigit,0,0);
+			group.transform.localPosition=new Vector3(-i*marginDigit,0,0);
 			for(int j=0;j<10;j++){
-				var ins=Instantiate(prefab) as UI.Text;
+				var ins=Instantiate(prefab) as UnityEngine.UI.Text;
 				ins.transform.parent=group.transform;
-				ins.transform.localPosition=new Vecto3(0,j*marginNumber,0);
+				ins.text=j.ToString();
+				ins.name=ins.text;
+				// ins.transform.localPosition=new Vector3(0,j*marginNumber,0);
 			}
+			var indexer=group.AddComponent<TrnthFxIndexerTranslate>();
+			indexer.length=10;
+			indexer.margin=marginNumber;
+			indexer.setup();
+			list.Add(indexer);
 		}
+		var slidingNumber=GetComponent<TrnthFxSlidingNumber>();
+		slidingNumber.digits=list.ToArray();
 	}
 } 
