e7ded26962fa5403a223e679a3023d7556ef57a4
JimbowMBPR <adeathjohn@icloud.com>
2015-04-15 00:19:46 +0800

    delete obsolete

--- TrnthFSM.cs
+++ TrnthFSM.cs
@@ -2,30 +2,10 @@
 using System.Collections;
 using System.Linq;
 
-public class TrnthFSM_ : MonoBehaviour {
-	// static public void transit(GameObject gameObject){
-	// 	var fsmManager=state.transform.parent.GetComponent<TrnthFSMManager>();
-	// 	fsmManager.stateNow=state;
-	// 	fsmManager.update();
-	// }
-	public GameObject stateNow;
-	[ContextMenu("update")]
-	public virtual void update(){
-		transit(stateNow);
-	}
-	public virtual void transit(GameObject gameObject){
-		stateNow=gameObject;
-		foreach(Transform e in transform.Cast<Transform>().ToArray()){
-			e.gameObject.SetActive(e.gameObject==stateNow);
+public class TrnthFSM : TrnthVariable { 
+	static public void transit(Transform state){
+		foreach(Transform e in state.parent.Cast<Transform>().ToArray()){
+			e.gameObject.SetActive(e==state);
 		}
-	}
-	public virtual void transit(string name){
-		foreach(Transform e in transform.Cast<Transform>().ToArray()){
-			if(e.name==name)stateNow=e.gameObject;
-			e.gameObject.SetActive(e.name==name);			
-		}
-	}
-	void Start(){
-		transit(stateNow);
 	}
 }
--- TrnthHVSActionAnimator.cs
+++ TrnthHVSActionAnimator.cs
@@ -3,10 +3,10 @@ using System.Collections;
 
 public class TrnthHVSActionAnimator : TrnthHVSAction {
 	public Animator animator;
-	public TrnthAnimatorProxy proxy;
+	// public TrnthAnimatorProxy proxy;
 	public string parameterName="speed";
 	public void setup(){
-		if(proxy)animator=proxy.animator;
+		// if(proxy)animator=proxy.animator;
 		if(!animator)animator=GetComponent<Animator>();
 	}
 	protected override void _execute(){
