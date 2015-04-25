279088f4b5f8fef4399373f4e19e32d19fd8fea5
JimbowMBPR <adeathjohn@icloud.com>
2015-04-25 17:29:22 +0800

    extenshion

---
+++ TrnthExtensions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TRNTH{
+public static class TrnthExtensions
+{
+    public static T find(this IEnumerable<T> list,T value)
+    {
+        foreach(T e in list){
+        	if(value==e)return e;
+        }
+        return default(T);
+    }
+}   
+}
