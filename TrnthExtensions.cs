9b0d1364dffbc74644cf285c2fdf663f614518b5
JimbowMBPR <adeathjohn@icloud.com>
2015-04-26 16:26:20 +0800

    extension

---
+++ TrnthExtensions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TRNTH{
+public static class TrnthExtensions
+{
+    public static T find<T>(this IEnumerable<T> list,T value) where T : class
+    {
+        foreach(T e in list){
+        	if(value==(e))return e;
+        }
+        return default(T);
+    }
+    public static T choose<T>(this IList<T> list){
+    	if(list.Count<1)return default(T);
+		return list[Random.Range(0,list.Count)];
+    }
+    public static void send(this MonoBehaviour monoBehaviour,TrnthHVSCondition condition){
+    	if(condition)condition.send();
+    }
+}   
+}
