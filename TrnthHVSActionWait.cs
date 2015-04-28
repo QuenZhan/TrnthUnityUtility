1d13ab11d495ca168b690db08086dc1f9fccb6a7
JimbowMBPR <adeathjohn@icloud.com>
2015-04-28 19:59:05 +0800

    william state laugh

--- TrnthHVSActionWait.cs
+++ TrnthHVSActionWait.cs
@@ -3,12 +3,13 @@ using System.Collections;
 
 public class TrnthHVSActionWait : TrnthHVSAction {
 	public float Delay=1;
+	public float noise=0;
 	public bool cancelOnDisable;
 	public TrnthHVSCondition onTimesUp;
 	// public TrnthHVSCondition onCancel;
 	protected override void _execute(){
 		base._execute();
-		if(enabled||!cancelOnDisable)Invoke("timesup",Delay);
+		if(enabled||!cancelOnDisable)Invoke("timesup",Delay+Random.value*noise);
 	}
 	void timesup(){
 		onTimesUp.send();
