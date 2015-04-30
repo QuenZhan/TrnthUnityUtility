91c1083ebacf33e3c2a371524cbff9ccbcac6498
JimbowMBPR <adeathjohn@icloud.com>
2015-05-01 02:39:22 +0800

    minion despot on invisible

---
+++ TrnthHVSActionRendererIsVisible.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using TRNTH;
+
+public class TrnthHVSActionRendererIsVisible : TrnthHVSAction {
+	public Renderer renderer;
+	public TrnthHVSCondition onTrue;
+	public TrnthHVSCondition onFalse;
+	protected override void _execute(){
+		base._execute();
+		if(renderer.isVisible)send(onTrue);
+		else send(onFalse);
+	}
+}
