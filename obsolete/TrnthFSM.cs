04c1374de16e74901aa68b96fe79493275876177
JimbowMBPR <adeathjohn@icloud.com>
2015-05-06 20:30:03 +0800

    outro

--- obsolete/TrnthFSM.cs
+++ obsolete/TrnthFSM.cs
@@ -2,7 +2,10 @@
 using System.Collections;
 using System.Linq;
 
-public class TrnthFSM : TrnthVariable { 
+public class TrnthFSM : TrnthVariable { 	
+	static public void transit(GameObject state){
+		transit(state.transform);
+	}
 	static public void transit(Transform state){
 		foreach(Transform e in state.parent.Cast<Transform>().ToArray()){
 			e.gameObject.SetActive(e==state);
