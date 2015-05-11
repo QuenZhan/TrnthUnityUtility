689f4a51e58ece55c689d7382cec48d893d9c87e
JimbowMBPR <adeathjohn@icloud.com>
2015-05-11 16:10:57 +0800

    pooling 優化

--- TrnthHVSConditionEnable.cs
+++ TrnthHVSConditionEnable.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class TrnthHVSConditionEnable : TrnthHVSCondition {
 	void OnEnable(){
+		// if(_afterAwake&&notBeforeAwake)send();
+		// if(!notBeforeAwake||_afterAwake)send();
 		send();
 	}
 }
