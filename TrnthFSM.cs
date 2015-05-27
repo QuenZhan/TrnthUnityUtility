d4f61dc03c2e7d85ad2a5d6bca16f462e600cfe3
JimbowMBPR <adeathjohn@icloud.com>
2015-05-27 13:50:56 +0800

    完成修改！射擊的部分okay

--- TrnthFSM.cs
+++ TrnthFSM.cs
@@ -2,30 +2,14 @@
 using System.Collections;
 using System.Linq;
 
-public class TrnthFSM_ : MonoBehaviour {
-	// static public void transit(GameObject gameObject){
-	// 	var fsmManager=state.transform.parent.GetComponent<TrnthFSMManager>();
-	// 	fsmManager.stateNow=state;
-	// 	fsmManager.update();
-	// }
-	public GameObject stateNow;
-	[ContextMenu("update")]
-	public virtual void update(){
-		transit(stateNow);
+public class TrnthFSM : MonoBehaviour { 	
+	static public void transit(GameObject state){
+		transit(state.transform);
 	}
-	public virtual void transit(GameObject gameObject){
-		stateNow=gameObject;
-		foreach(Transform e in transform.Cast<Transform>().ToArray()){
-			e.gameObject.SetActive(e.gameObject==stateNow);
+	static public void transit(Component comp){
+		var state=comp.transform;
+		foreach(Transform e in state.parent.Cast<Transform>().ToArray()){
+			e.gameObject.SetActive(e==state);
 		}
-	}
-	public virtual void transit(string name){
-		foreach(Transform e in transform.Cast<Transform>().ToArray()){
-			if(e.name==name)stateNow=e.gameObject;
-			e.gameObject.SetActive(e.name==name);			
-		}
-	}
-	void Start(){
-		transit(stateNow);
 	}
 }
