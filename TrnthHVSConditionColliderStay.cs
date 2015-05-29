a42cd56278c07ec7b05d58aced6c3725a955ecfa
JimbowMBPR <adeathjohn@icloud.com>
2015-05-29 15:24:17 +0800

    吹飛修正

---
+++ TrnthHVSConditionColliderStay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+public class TrnthHVSConditionColliderStay : TrnthHVSConditionCollider {	
+	void OnTriggerStay(Collider collider){
+		if(debugLog)Debug.Log(name,collider);
+		if(!includeTrigger)return;
+		// _col=collider;
+		sendFilter(collider);
+	}
+	void OnCollisionStay(Collision collision){
+		if(debugLog)Debug.Log(name,collision.collider);
+		// _col=collision.collider;
+		sendFilter(collision.collider);
+	}
+}
