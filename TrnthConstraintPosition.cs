eb34b41efb33789884fefa8e378b9e71f0cd9fae
JimbowMBPR <adeathjohn@icloud.com>
2015-06-02 01:41:51 +0800

    position , on fire fx

--- TrnthConstraintPosition.cs
+++ TrnthConstraintPosition.cs
@@ -3,14 +3,9 @@ using System.Collections;
 
 public class TrnthConstraintPosition : TrnthConstraint {
 	public Transform position;
-	public override void setup(){
-		base.setup();
-		var variable=GetComponent<TrnthVariable>();
-		if(variable)position=variable.read<Transform>();
-	}
 	public override void update () {
 		base.update();
-		if(!position)setup();
-		target.position=position.position;
+		// if(!position)setup();
+		if(position)target.position=position.position;
 	}
 }
--- TrnthHVSActionAttackSender.cs
+++ TrnthHVSActionAttackSender.cs
@@ -6,6 +6,7 @@ public class TrnthHVSActionAttackSender : TrnthHVSAction {
 	public TrnthHVSActionPhysicsCast pc;
 	public TrnthHVSConditionCollider conditionCollider;
 	public TrnthAttack attack;
+	public TrnthHVSActionSpawn[] attachments;
 	protected override void _execute(){
 		base._execute();
 		// if(proxy)pc=proxy.physicsCast;
@@ -17,6 +18,11 @@ public class TrnthHVSActionAttackSender : TrnthHVSAction {
 			// if(!dr)dr=GetComponent<TrnthHVSConditionAttackReceiver>();
 			if(!dr)continue;
 			dr.hurtWith(attack,pc);
+			foreach(var spawner in attachments){
+				spawner.execute();
+				var constraint=spawner.spawned.gameObject.AddComponent<TrnthConstraintPosition>();
+				constraint.position=e.transform;
+			}
 		}
 	}
 	// Collider[] colliders=new Collider[0];
