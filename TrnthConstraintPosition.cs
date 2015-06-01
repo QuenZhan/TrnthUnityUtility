4ff997b9e321776b6dff2528471e8eced70993f9
JimbowMBPR <adeathjohn@icloud.com>
2015-06-02 02:15:16 +0800

    team ally lad bug fixed

--- TrnthConstraintPosition.cs
+++ TrnthConstraintPosition.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System;
 public class TrnthConstraintPosition : TrnthConstraint {
 	public Transform position;
+	public event Action<TrnthConstraintPosition> onLosingPosition=delegate{};
 	public override void update () {
 		base.update();
 		// if(!position)setup();
 		if(position)target.position=position.position;
+		else {
+			enabled=false;
+			onLosingPosition(this);
+		}
 	}
 }
--- TrnthHVSActionAttackSender.cs
+++ TrnthHVSActionAttackSender.cs
@@ -6,7 +6,6 @@ public class TrnthHVSActionAttackSender : TrnthHVSAction {
 	public TrnthHVSActionPhysicsCast pc;
 	public TrnthHVSConditionCollider conditionCollider;
 	public TrnthAttack attack;
-	public TrnthHVSActionSpawn[] attachments;
 	protected override void _execute(){
 		base._execute();
 		// if(proxy)pc=proxy.physicsCast;
@@ -18,11 +17,7 @@ public class TrnthHVSActionAttackSender : TrnthHVSAction {
 			// if(!dr)dr=GetComponent<TrnthHVSConditionAttackReceiver>();
 			if(!dr)continue;
 			dr.hurtWith(attack,pc);
-			foreach(var spawner in attachments){
-				spawner.execute();
-				var constraint=spawner.spawned.gameObject.AddComponent<TrnthConstraintPosition>();
-				constraint.position=e.transform;
-			}
+			attack.attach(e.transform);
 		}
 	}
 	// Collider[] colliders=new Collider[0];
