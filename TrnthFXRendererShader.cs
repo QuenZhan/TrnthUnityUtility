fb281a88b69d34c65a5bef71f75784674347e2f2
JimbowMBPR <adeathjohn@icloud.com>
2015-06-01 14:30:58 +0800

    完成被打時白色覆蓋的閃爍效果

---
+++ TrnthFXRendererShader.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TrnthFXRendererShader : MonoBehaviour {
+	public Renderer rdr;
+	public Shader shader;
+	public float duration=0.1f;
+	public virtual void start(){
+		memorize();
+		var list=new List<Material>();
+		foreach(var e in rdr.materials){
+			e.shader=shader;
+		}
+		Invoke("end",duration);
+	}
+	protected virtual void end(){
+		foreach(var e in rdr.materials){
+			e.shader=dic[e];
+		}
+		enabled=false;
+	}
+	Dictionary<Material,Shader> dic=new Dictionary<Material,Shader>();
+	void memorize(){
+		dic.Clear();
+		foreach(var e in rdr.materials){
+			dic.Add(e,e.shader);
+		}
+	}
+	void OnEnable(){
+		start();
+	}
+
+}
