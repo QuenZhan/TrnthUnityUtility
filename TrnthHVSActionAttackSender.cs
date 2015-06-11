cc0d94e64efb2d7a50263983344291a4638d2f9c
JimbowMBPR <adeathjohn@icloud.com>
2015-06-12 02:46:22 +0800

    attack system trimming

--- TrnthHVSActionAttackSender.cs
+++ TrnthHVSActionAttackSender.cs
@@ -5,20 +5,37 @@ using System.Collections.Generic;
 public class TrnthHVSActionAttackSender : TrnthHVSAction {
 	public TrnthHVSActionPhysicsCast pc;
 	public TrnthHVSConditionCollider conditionCollider;
-	public TrnthAttack attack;
+	public TrnthHVSCondition conditionReact;
+	public Transform direction;
+	public float damageBase=30;
+	public float knockback;
+	public bool showDamage=false;
+	public TrnthHVSActionSpawn[] attachments;
+
+	public event System.Action<TrnthHVSActionAttackSender,TrnthHVSConditionAttackReceiver> onReact=delegate{};
+	public virtual float damage{get{
+		return 1+Random.value*(damageBase);
+	}}
 	protected override void _execute(){
 		base._execute();
-		// if(proxy)pc=proxy.physicsCast;
 		var colliders=new Collider[0];
 		if(pc)colliders=pc.colliders;
 		if(conditionCollider)colliders=new Collider[]{conditionCollider.col};
 		foreach(Collider e in colliders){
-			attack.attach(e.transform);
+			attach(e.transform);
 			var dr=e.GetComponent<TrnthHVSConditionAttackReceiver>();
-			// if(!dr)dr=GetComponent<TrnthHVSConditionAttackReceiver>();
 			if(!dr)continue;
-			dr.hurtWith(attack,pc);
+			dr.hurtWith(this,react);
 		}
 	}
-	// Collider[] colliders=new Collider[0];
+	protected virtual void attach(Transform tra){
+		foreach(var spawner in attachments){
+			spawner.transform.position=tra.position;
+			spawner.execute();
+		}
+	}
+	void react(TrnthHVSConditionAttackReceiver receiver){
+		this.send(conditionReact);
+		onReact(this,receiver);
+	}
 }
--- TrnthHVSConditionAttackReceiver.cs
+++ TrnthHVSConditionAttackReceiver.cs
@@ -5,47 +5,22 @@ public class TrnthHVSConditionAttackReceiver :  TrnthHVSCondition {
 
 	public float damage{get;private set;}
 	public float hpBeforeHit{get;private set;}
-	public TrnthAttack attack{get;private set;}
+	public TrnthHVSActionAttackSender attack{get;private set;}
 	public TrnthRadio hp;
 	public Transform direction;
-	[HideInInspector]public TrnthHVSCondition onHurt;
-	[HideInInspector]public TrnthHVSCondition onDie;
-	[HideInInspector]public TrnthHVSCondition onKnockback;
 	public bool persistent;
-	public virtual void hurtWith(TrnthAttack attack,TrnthHVSActionPhysicsCast physicsCast){
+	public virtual void hurtWith(TrnthHVSActionAttackSender attack,System.Action<TrnthHVSConditionAttackReceiver> react){
 		this.attack=attack;
 		damage=attack.damage;
 		hpBeforeHit=hp.value;
 		if(direction){
 			direction.transform.position=transform.position;
-			direction.LookAt(attack.transform);
+			direction.LookAt(attack.direction.transform);
 		}
-		conditionSend();
 		hp-=damage;
 		if(persistent&&hpBeforeHit>1&&hp.value<1)hp.value=1;
-
 		hp.clamp();
-		attack.react(damage);
+		react(this);
 		send();
-		log();
 	}
-	public virtual void conditionSend(){
-		var isDead=damage>hp.value;
-		if(persistent)isDead=damage>hp.value&&hp.rate==0;
-		if(isDead){
-			// if(toDie){
-			// 	toDie.execute();
-			// }
-			if(onDie)onDie.send();
-		}else{
-			if(attack.knockback){
-				// if(knockback)knockback.execute();
-				if(onKnockback)onKnockback.send();
-			}else{
-				// if(toHurt)toHurt.execute();
-				if(onHurt)onHurt.send();			
-			}
-		}
-	}
-
 }
