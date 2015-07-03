c07b8036c227ab0c3bcf360f525b76e67fc939e6
JimbowMBPR <adeathjohn@icloud.com>
2015-07-03 14:29:51 +0800

    音效更新版

--- obsolete/TrnthRandomChildActivate.cs
+++ obsolete/TrnthRandomChildActivate.cs
@@ -7,9 +7,10 @@ public class TrnthRandomChildActivate : MonoBehaviour {
 		if(!target)target=transform;
 	}
 	void OnEnable(){
-		foreach(Transform e in target){
-			e.gameObject.SetActive(false);
-		}
-		TRNTH.U.choose(target).gameObject.SetActive(true);
+		// foreach(Transform e in target){
+		// 	e.gameObject.SetActive(false);
+		// }
+		TrnthFSM.transit(TRNTH.U.choose(target));
+		// TRNTH.U.choose(target).gameObject.SetActive(true);
 	}
 }
