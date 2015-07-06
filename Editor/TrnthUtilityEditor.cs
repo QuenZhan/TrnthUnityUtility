39203607a09142de4d1a5881bac944b8ea3ef8ae
JimbowMBPR <adeathjohn@icloud.com>
2015-07-07 00:17:39 +0800

    transit update

--- Editor/TrnthUtilityEditor.cs
+++ Editor/TrnthUtilityEditor.cs
@@ -1,12 +1,12 @@
 ﻿using UnityEngine;
 using UnityEditor;
 public class TrnthUtilityEditor : Editor {
-	[MenuItem("TRNTH/DeletePlayerPrefs %&d")]
+	[MenuItem("TRNTH/DeletePlayerPrefs")]
     private static void deletePrefs() {
     	PlayerPrefs.DeleteAll();
     	Debug.Log("PlayerPrefs.DeleteAll() ");
     }
-    [MenuItem("TRNTH/Isolate %&i")]
+    [MenuItem("TRNTH/Isolate %i")]
     static void isolate(){
     	TrnthFSM.transit(Selection.activeGameObject);
     	// ;
--- TrnthFSM.cs
+++ TrnthFSM.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class TrnthFSM:MonoBehaviour {
+public class TrnthFSM {
 	static TrnthFSM instance;
 	static public void transit(GameObject state){
 		transit(state.transform);
@@ -13,31 +13,34 @@ public class TrnthFSM:MonoBehaviour {
 		foreach(Transform e in parent){
 			e.gameObject.SetActive(e==state);
 		}
-		// if(!instance){
-			// instance=(new GameObject("TrnthFSM")).AddComponent<TrnthFSM>();
-		// }
-		// instance.checkDelta(state);
+		#if UNITY_EDITOR
+			if(instance==null){
+				instance=(new TrnthFSM());
+			}
+			instance.checkDelta(state);
+		#endif
 	}
 	static public void clear(Component comp){
 		foreach(Transform e in comp.transform.parent){
 			e.gameObject.SetActive(false);
 		}
 	}
-	List<Data> parents=new List<Data>();
 	internal void checkDelta(Transform child){
-		StartCoroutine(_checkDelta(child));
-	}
-	IEnumerator _checkDelta(Transform child) {
 		var parent=child.parent;
 		foreach(var data in parents.ToArray()){
+			if(Time.time-data.time>MINTIME){
+				parents.Remove(data);
+				continue;
+				// continue;
+			}
 			if(data.parent!=parent)continue;
-			// Debug.LogWarning(System.String.Format("TrnthFSM delta: {0}/{1} >> {2}/{3}",data.parent.name,data.child.name,parent.name,child.name),child);
+			// Debug.LogWarning(System.String.Format("TrnthFSM delta time :{4} . {0}/{1} -> {2}/{3}",data.parent.name,data.child.name,parent.name,child.name,Time.time-data.time),child);
 		}
 		var _data=new Data(){time=Time.time,parent=parent,child=child};
 		parents.Add(_data);
-        yield return new WaitForSeconds(0.1f);
-        parents.Remove(_data);
-    }
+	}
+	const float MINTIME=0.1f;
+	List<Data> parents=new List<Data>();
     class Data{
     	public float time;
     	public Transform parent;
