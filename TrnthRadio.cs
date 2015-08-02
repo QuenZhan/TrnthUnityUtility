a5ffb088b0b61ee883726f588c70ef9aaa121202
JimbowMBPR <adeathjohn@icloud.com>
2015-08-03 01:50:46 +0800

    ui update

--- TrnthRadio.cs
+++ TrnthRadio.cs
@@ -3,12 +3,13 @@
 public class TrnthRadio:MonoBehaviour,ITrnthRadioGet{
 	[SerializeField]float _length=100;
 	// [SerializeField]float _rate=1;
-	public float length{get{return _length;}set{_length=value;}}
+	public float length{get{return _length;}set{_length=value;onChanged(this);}}
 	public float rate{
 		get{
 			return _value/_length;
 		}set{
 			_value=value*_length;
+			onChanged(this);
 		}
 	}
 	public bool fullOnEnable=true;
@@ -24,6 +25,7 @@ public class TrnthRadio:MonoBehaviour,ITrnthRadioGet{
 		set{
 			if(value)rate=1f;
 			else rate=0f;
+			onChanged(this);
 		}
 	}
 	public float value{
@@ -31,15 +33,18 @@ public class TrnthRadio:MonoBehaviour,ITrnthRadioGet{
 			return _value;
 		}set{
 			_value=value;
+			onChanged(this);
 		}
 	}
 	public void clamp(){
 		if(rate<0)rate=0;
 		if(rate>1)rate=1;
+		onChanged(this);
 	}
 	public string stringPercent(){
 		return (int)(rate*100)+"%";
 	}
+	public event System.Action<TrnthRadio> onChanged=delegate{};
 	float _value;
 	void OnEnable(){
 		if(fullOnEnable)toggle=true;
