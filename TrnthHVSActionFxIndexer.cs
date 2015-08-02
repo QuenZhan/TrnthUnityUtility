e97c88ca50a0a8b5fdc12ab9e7f467e19fe38215
JimbowMBPR <adeathjohn@icloud.com>
2015-08-02 09:50:13 +0800

    indexer

---
+++ TrnthHVSActionFxIndexer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrnthHVSActionFxIndexer : TrnthHVSAction {
+	[SerializeField]TrnthFxIndexer indexer;
+	[SerializeField]int index;
+	protected override void _execute(){
+		indexer.index=index;
+	}
+}
