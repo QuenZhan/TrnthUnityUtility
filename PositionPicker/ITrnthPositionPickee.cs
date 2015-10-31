6d53ca5fe63214fd57f23d3a01299f8fd905b7a4
JimbowMBPR <adeathjohn@icloud.com>
2015-10-31 13:18:00 +0800

    position picker

---
+++ PositionPicker/ITrnthPositionPickee.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface ITrnthPositionPickee{
+	Vector3 positionWorld{get;}
+	void onPosition(ITrnthPositionPicker picker);
+	void onAwayPosition(ITrnthPositionPicker picker);
+}
--- UI/ITrnthUIScrollPickee.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public interface ITrnthUIScrollPickee  {
-	void pick(TrnthUIScrollPicker picker);
-}
