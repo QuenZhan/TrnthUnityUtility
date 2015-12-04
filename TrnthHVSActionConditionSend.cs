2555774c86552cbea5fb7b707a733e8a398a935e
JimbowMBPR <adeathjohn@icloud.com>
2015-12-04 23:30:57 +0800

    boss william Fixed

--- TrnthHVSActionConditionSend.cs
+++ TrnthHVSActionConditionSend.cs
@@ -3,13 +3,11 @@ using System.Collections;
 
 public class TrnthHVSActionConditionSend : TrnthHVSAction {
 	public TrnthHVSCondition condition;
-	public string find;
+	[HideInInspector]public string find;
 	protected override void _execute(){
 		base._execute();
 		if(!condition){
 			Debug.LogError("!condition",this);
-			// var go=GameObject.Find(find);
-			// condition=go.GetComponent<TrnthHVSCondition>();
 		}
 		condition.send();
 	}
