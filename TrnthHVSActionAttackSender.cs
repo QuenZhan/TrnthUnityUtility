433994b8d21e7748972baf11259c7edc0a9f0e3f
JimbowMBPR <jimbow@crespirit.com>
2016-02-15 04:10:46 +0800

    改寫一些介面

--- TrnthHVSActionAttackSender.cs
+++ TrnthHVSActionAttackSender.cs
@@ -7,7 +7,7 @@ public class TrnthHVSActionAttackSender : TrnthHVSAction {
 	public TrnthHVSConditionCollider conditionCollider;
 	public TrnthAttack attack;
 	protected override void _execute(){
-		OnEnable();
+		// OnEnable();
 		base._execute();
 		var colliders=new Collider[0];
 		if(pc)colliders=pc.colliders;
@@ -20,26 +20,6 @@ public class TrnthHVSActionAttackSender : TrnthHVSAction {
 			var dr=e.GetComponent<DSAttackReceiver>();
 			if(!dr)continue;
 			dr.hurtWith(attack.report,attack as IDSOffensive);
-		}
-	}
-	protected void OnEnable(){
-		var dsAttack=(attack as DSMAttack);
-		if(!dsAttack
-			||dsAttack.shell==null
-			||dsAttack.shell.member==null
-			||dsAttack.shell.member.team==null
-			||dsAttack.shell.team==null
-			)return;
-		if(pc){
-			pc.layermask=1<<dsAttack.shell.team.enemy.layerReciever;
-			pc.transform.localPosition=Vector3.right*dsAttack.attack.distance;
-			pc.distance=0;
-			pc.radius=dsAttack.attack.radius;
-		}
-		if(conditionCollider){
-			// var col=conditionCollider.GetComponent<Collider>();
-			conditionCollider.gameObject.layer=dsAttack.shell.member.team.layerCaster;
-			// col.
 		}
 	}
 }
