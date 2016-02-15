bf88fe8b3b8079916b87d5db4cf3a93ffaab3d07
JimbowMBPR <jimbow@crespirit.com>
2016-02-16 01:31:53 +0800

    重力手雷完成

--- TrnthQuality.cs
+++ TrnthQuality.cs
@@ -2,6 +2,10 @@
 using System.Collections;
 
 public class TrnthQuality : MonoBehaviour {
+	// [SerializeField]
+	public void onChange(float index){
+		// setQuality((int)index);
+	}
 	public void setQuality(int index){
 		QualitySettings.SetQualityLevel(index,true);
 	}
