9345e541302c460bd0724cfdbf0ed2b55a55ea63
JimbowMBPR <jimbow@crespirit.com>
2016-02-18 00:11:46 +0800

    更新 main ui

---
+++ TrnthHVSActionActivateOff.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrnthHVSActionActivateOff : TrnthHVSAction {
+	[SerializeField]GameObject _target;
+	protected override void _execute(){
+		base._execute();
+		_target.SetActive(false);
+	}
+}
