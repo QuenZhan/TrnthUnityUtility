66d9b8d83a7f12f6244ab8378c11ca3f38e7dc8a
JimbowMBPR <jimbow@crespirit.com>
2016-03-24 23:49:52 +0800

    battle result upgrade , story  3 update

---
+++ TrnthAnimationEvent.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrnthAnimationEvent : MonoBehaviour {
+	public event System.Action<TrnthAnimationEvent> onEvent=delegate{};
+	void evetnTriggers(){
+		onEvent(this);
+	}
+}
