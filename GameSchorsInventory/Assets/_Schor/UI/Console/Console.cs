a39c3139f53590c4a2d606b04928f8d84e60f669
JImbo <jimbow@crespirit.com>
2017-08-11 13:38:09 +0800

    moving folder

---
+++ GameSchorsInventory/Assets/_Schor/UI/Console/Console.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace TRNTH.SchorsInventory.UI.Component{
+	public class Console : MonoBehaviour
+	{
+			public void Log(string content0,string conten1,string conten2){
+
+			}
+
+	}
+
+}
---
+++ GameSchorsInventory/Assets/_Schor/UI/Console/ConsoleRow.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace TRNTH.SchorsInventory.UI.Component{
+
+    public class ConsoleRow : MonoBehaviour, IListCell
+    {
+        int IListCell.Index {get;set;}
+    }
+}
