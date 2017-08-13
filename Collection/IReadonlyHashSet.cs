696c58ac3c6e7e9bd92c7a683d2ecf91173b2711
JImbo <jimbow@crespirit.com>
2017-08-13 18:32:50 +0800

    faction filter rewrite

---
+++ Collection/IReadonlyHashSet.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace TRNTH{
+	public interface IReadonlyHashSet<T>  {
+		bool Contains(T item);
+	}
+	public class HashSet<T>:System.Collections.Generic.HashSet<T>,IReadonlyHashSet<T>{
+
+	}
+}
