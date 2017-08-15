20172502bc541f61dfefb474a8402c8ff08cbef2
JImbo <jimbow@crespirit.com>
2017-08-15 11:17:12 +0800

    Squashed 'GameSchorsInventory/Assets/Trnth/' changes from 3a62926..a53bb9d
    a53bb9d 匯入特效差件，新增揮砍擊中特效
    f500941 改變 pool 底層
    ae40ba9 Merge commit '19c2fccfab3fbc3fdaee0d2fc1706759f0735734' into jimbo
    dd7f203 utility update
    696c58a faction filter rewrite
    15ccc26 Merge commit '9a73d17066e457968892be62d2565a7afb1e18f4' into jimbo
    13f1e53 Merge commit '61401d08e03198203fb6c61767591a77f78a688b'
    5d3ad65 moving folder
    
    git-subtree-dir: GameSchorsInventory/Assets/Trnth
    git-subtree-split: a53bb9d160880f642826b58cf0279aa48258e465

--- Pool/Pool.cs
+++ Pool/Pool.cs
@@ -6,51 +6,19 @@ using System.Collections.ObjectModel;
 
 namespace TRNTH.Pooling{
 	[System.Serializable]
-	public class Pool<T> where T:Component{
-		public T Prefab;
-		public int Limiation=3;
-		[ContextMenuItem("PreSpawn", "PreSpawn")]
+	public class Pool<T> {
 		[SerializeField]List<T> _Instances=new List<T>();
-		public ReadOnlyCollection<T> Components{
+		public IReadOnlyList<T> Instances{
 			get{
-				if(_readonlyComponents==null)_readonlyComponents=_Instances.AsReadOnly();
-				return _readonlyComponents;
+				return _Instances;
 			}
 		}
-		ReadOnlyCollection<GameObject> _radonlyRoots;
-		ReadOnlyCollection<Transform> _readonlyTransforms;
-		ReadOnlyCollection<T> _readonlyComponents;
-		
-		public int SpawningIndex{get;private set;}
-		public virtual void PreSpawn(){
-			foreach(var e in _Instances){
-				Object.DestroyImmediate(e);
-			}
-			_Instances.Clear();
-			for(var i=0;i<Limiation;i++){
-				_Instances.Add(Object.Instantiate(Prefab));
-				PreSpawned(i,_Instances[i].gameObject);
-			}
-		}
-		protected virtual void PreSpawned(int index,GameObject newGo){
-			newGo.SetActive(false);
-		}
-		public virtual int Spawn(){
-			if(_Instances==null 
-			||  _Instances.Count!=Limiation 
-			|| _Instances[0]==null
-			){
-				PreSpawn();
-//				throw new System.InvalidOperationException("PreSpawn first");
-			}
+		int SpawningIndex;
+		public T Spawn(){
+			var Limiation=_Instances.Count;
 			SpawningIndex=++SpawningIndex%Limiation;
-			var go=_Instances[SpawningIndex];
-			go.gameObject.SetActive(true);
-			return SpawningIndex;
-		}
-		public T SpawnAndGetInstance(){
-			var index=Spawn();
-			return this.Components[index];
+			var index=SpawningIndex;
+			return _Instances[index];
 		}
 	}
 }
