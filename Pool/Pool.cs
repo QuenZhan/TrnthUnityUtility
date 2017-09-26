b4ffb1fa07e670eda2a2a1ad381113ba063cbab0
JImbo <jimbow@crespirit.com>
2017-09-26 13:37:22 +0800

    food chain

--- Pool/Pool.cs
+++ Pool/Pool.cs
@@ -7,6 +7,7 @@ using System.Collections.ObjectModel;
 namespace TRNTH.Pooling{
 	[System.Serializable]
 	public class Pool<T> {
+		
 		[SerializeField]List<T> _Instances=new List<T>();
 		public IReadOnlyList<T> Instances{
 			get{
@@ -19,6 +20,14 @@ namespace TRNTH.Pooling{
 			SpawningIndex=++SpawningIndex%Limiation;
 			var index=SpawningIndex;
 			return _Instances[index];
+		}
+	}
+	public class ComponentsPool<T> :Pool<T> where T:Component{
+		public void UnparentAll(){
+			for (int i = 0; i < Instances.Count; i++)
+			{
+				Instances[i].transform.parent=null;
+			}
 		}
 	}
 }
