25c418f143c2c3ca0cd1847f21ab75092e856df2
Jimbo <adeathjohn@gmail.com>
2018-01-14 16:00:21 +0800

    Scene Manager  changed , new iNonAllocList  , dodge hurt effect 玩家死亡後一陣子不再突然消失，衝刺傷害改為 ontrigger enter，滑鼠移到 dock 不再無法用鍵盤攻擊，蝸牛不再無屍體，箱子可以存檔、廚具內容可以存檔，飛行道具剛發射時不會被攻擊，不再有餓死UI，吃炸蝦球可以使用圓水嶄，西米講話不再被中斷

--- Collection/IReadonlyHashSet.cs
+++ Collection/IReadonlyHashSet.cs
@@ -9,9 +9,31 @@ namespace TRNTH{
 	public class HashSet<T>:System.Collections.Generic.HashSet<T>,IReadonlyHashSet<T>{
 
 	}
-	// public interface IReadOnlyList<T>{
-	// 	T this[int index]{get;}
-	// 	int Count{get;}
-	// }
-	// public class List<T>:System.Collections.Generic.List<T>,IReadOnlyList<T>{}
+	public interface IReadOnlyNonAllocList<T>{
+		T this[int index]{get;}
+		int Count{get;}
+	}
+	public interface INonAllocList<T>:IReadOnlyNonAllocList<T>{
+		new T this[int index]{get;set;}
+	}
+}
+namespace TRNTH.ContainerExtention{
+	static class Extension{
+		static public void Clear<T>(this INonAllocList<T> list){
+			var length=list.Count;
+			for (int i = 0; i < length; i++)
+			{
+				list[i]=default(T);
+			}
+		}
+		static public int CopyTo<T>(this IReadOnlyNonAllocList<T> from,INonAllocList<T> to){
+			var length=from.Count;
+			if(to.Count<length)length=to.Count;
+			for (int i = 0; i < length; i++)
+			{
+				to[i]=from[i];
+			}
+			return length;
+		}
+	}
 }
