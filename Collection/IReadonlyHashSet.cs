d3076a1ba005a557018ff5d838398242614a4930
Jimbo <adeathjohn@gmail.com>
2018-01-15 12:19:34 +0800

    - [x] 需要滾輪裝備道具 - [x] 衝刺傷害的判定應改成 onTriggerEnter; - [x] 所有箱子都壞了 - [x] 爛光光 bug - [x] Crafter 的道具欄應該自動偵測、自動存檔 - [x] 炸蝦球 bind 圓水斬 - [x] 死亡 Buff 清空 - [x] 死掉重完之後應該回到地圖的中心 - [x] - [x] 取消西米復活點 - [x] 主角有時候會消失 - [x] 遊戲目的的變化 - [x] 取消飢餓死亡與一般死亡的差異，皆為一般死亡 - [x] 需要 Buff /胃分離，移動系的食物也列在Buff條會被擠掉。 - [x]  Simple Fish 是壞的 - [x] 魔法書會把自己射出的飛彈打爆 - [x] 打 grill 死亡，出生血沒有恢復成一半 - [x] 弓箭失效

--- Collection/IReadonlyHashSet.cs
+++ Collection/IReadonlyHashSet.cs
@@ -16,9 +16,41 @@ namespace TRNTH{
 	public interface INonAllocList<T>:IReadOnlyNonAllocList<T>{
 		new T this[int index]{get;set;}
 	}
+    public class NonAllocList<T> : INonAllocList<T>
+    {
+		[SerializeField]T[] _datas;
+		public NonAllocList():this(0){
+
+		}
+		public NonAllocList(int size){
+			_datas=new T[size];
+		}
+        public T this[int index]
+        {
+            get
+            {
+                return _datas[index];
+            }
+
+            set
+            {
+                _datas[index]=value;
+            }
+        }
+        public int Count {get{return _datas.Length;}}
+    }
 }
 namespace TRNTH.ContainerExtention{
 	static class Extension{
+		static public bool Contains<T>(this IReadOnlyNonAllocList<T> list,T item){
+			if(list==null)return false;
+			var length=list.Count;
+			for (int i = 0; i < length; i++)
+			{
+				if(list[i].Equals(item))return true;
+			}
+			return false;
+		}
 		static public void Clear<T>(this INonAllocList<T> list){
 			var length=list.Count;
 			for (int i = 0; i < length; i++)
