7e213b3a65415b7e19e1348e7a6590c1fb9645e5
Scofa <t1964723@gmail.com>
2018-02-27 02:37:13 +0800

    可build

--- Utility.cs
+++ Utility.cs
@@ -44,6 +44,7 @@ namespace TRNTH{
 	public class Utility{
 		[System.Diagnostics.Conditional("UNITY_EDITOR")]
 		public static void GetAllAssets<T>(IList<T> toHere) where T:class{
+			#if UNITY_EDITOR
 			var type=typeof(T);
 			var guids= UnityEditor.AssetDatabase.FindAssets(string.Format("t:{0}",type.Name));
 			toHere.Clear();
@@ -52,6 +53,7 @@ namespace TRNTH{
 				var recipe=UnityEditor.AssetDatabase.LoadAssetAtPath(path,type) as T;
 				toHere.Add(recipe);
 			}
+			#endif
 		}
 		static public void CheckSerializingType<T>(ref T member,ref MonoBehaviour monoScript) where T:class{
 			#if UNITY_EDITOR
