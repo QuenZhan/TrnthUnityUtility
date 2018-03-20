39d566b8050518d72b7784b70aab9f6e57872d9b
Jimbo <adeathjohn@gmail.com>
2018-03-20 21:18:54 +0800

    meta fucks

--- Editor/Brush.cs
+++ Editor/Brush.cs
@@ -5,7 +5,7 @@ namespace TRNTH{
 public class Brush : Editor {
 	void OnSceneGUI(){
 		HandleUtility.AddDefaultControl(GUIUtility.GetControlID(FocusType.Passive));
-		if(Event.current.type == EventType.mouseDown){
+		if(Event.current.type == EventType.MouseDown){
 			// hit as RaycastHit
 			if(Physics.Raycast(HandleUtility.GUIPointToWorldRay(Event.current.mousePosition)))
 				Debug.Log("Hit");
