e5469dcb50ce4a09642c065c982673255b992a54
Jimbo <adeathjohn@gmail.com>
2018-04-16 10:37:50 +0800

    photon network

--- TrnthUIClickOff.cs
+++ TrnthUIClickOff.cs
@@ -1,9 +1,26 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using System.Collections;
+// [RequireComponent(typeof(Button))]
+public class TrnthUIClickOff : MonoBehaviour,IPointerClickHandler,IPointerDownHandler,IPointerUpHandler {
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // throw new System.NotImplementedException();
+		gameObject.SetActive(false);
+    }
 
-public class TrnthUIClickOff : MonoBehaviour {
-	void Start () {
-		GetComponent<Button>().onClick.AddListener(()=>{gameObject.SetActive(false);});
-	}
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        // throw new System.NotImplementedException();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        // throw new System.NotImplementedException();
+    }
+
+    // void Start () {
+	// 	GetComponent<Button>().onClick.AddListener(()=>{gameObject.SetActive(false);});
+	// }
 }
