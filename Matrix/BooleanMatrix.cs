b48cb37d380cbd0c137d2096d7752b6daa9bac61
Jimbo <adeathjohn@gmail.com>
2018-04-16 10:36:28 +0800

    read me

--- Matrix/BooleanMatrix.cs
+++ Matrix/BooleanMatrix.cs
@@ -29,68 +29,6 @@ namespace TRNTH
             _height=height;
         }
     }
-    public class ArrayMatrix<T>:MatrixBase
-    ,ISerializationCallbackReceiver
-    ,IMatrix<T>
-    ,INonAllocList<T>
-    {
-        [SerializeField]MatrixIndex _currentIndex;
-        [SerializeField]T _current;
-        [SerializeField][HideInInspector]T[] _datas;
-        protected T[] Datas{get{return _datas;}}
-
-        public int Count
-        {
-            get
-            {
-                return _datas.Length;
-            }
-        }
-
-        public ArrayMatrix(int width, int height) : base(width, height)
-        {
-            _datas=new T[width*height];
-        }
-        public ArrayMatrix() : this(3, 3)
-        {
-        }
-        void ItemSet(int index,T item){
-			_datas[index]=item;
-        }
-
-        public void OnBeforeSerialize()
-        {
-            _current=this[_currentIndex];
-        }
-
-        public void OnAfterDeserialize()
-        {
-            // throw new NotImplementedException();
-        }
-
-        public T this[MatrixIndex vec] { 
-			get {
-				return _datas[vec.x+vec.y*Width];
-			}
-			set {
-                ItemSet(vec.x+vec.y*Width,value);
-			}
-		}
-        public T this[int index]{
-            get{return _datas[index];}
-            set{
-                _datas[index]=value;
-            }
-        }
-        public T this[int x,int y] { 
-			get {
-				return _datas[x+y*Width];
-			}
-			set {
-                ItemSet(x+y*Width,value);
-			}
-		}
-    }
     public interface IMatrix<T>:IReadonlyMatrix<T>{
         // void ChangeSize(int width,int height);
         new T this[int x,int y]{get;set;}
